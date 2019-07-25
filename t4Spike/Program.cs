2c70293ee107e38dd2946bd8d8ec239363a677d0
paulghodgson <paulghodgson@gmail.com>
2019-07-25 17:22:06 +0100

    first

---
+++ t4Spike/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t4Spike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started");
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+
+        }
+    }
+}
